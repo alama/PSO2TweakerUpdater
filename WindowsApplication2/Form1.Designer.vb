dc8f3ae32d4bc31367043392abf09d684eb7ce0a
TheCrimsonDevil <ZeroHeLL@GMail.com>
2015-01-15 08:21:46 -0500

    initial commit

---
+++ WindowsApplication2/Form1.Designer.vb
@@ -0,0 +1,70 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.LabelX1 = New DevComponents.DotNetBar.LabelX()
+        Me.ProgressBarX1 = New DevComponents.DotNetBar.Controls.ProgressBarX()
+        Me.SuspendLayout()
+        '
+        'LabelX1
+        '
+        '
+        '
+        '
+        Me.LabelX1.BackgroundStyle.Class = ""
+        Me.LabelX1.Location = New System.Drawing.Point(120, 12)
+        Me.LabelX1.Name = "LabelX1"
+        Me.LabelX1.Size = New System.Drawing.Size(196, 23)
+        Me.LabelX1.TabIndex = 0
+        Me.LabelX1.Text = "Downloading PSO2 Tweaker Updates..."
+        '
+        'ProgressBarX1
+        '
+        '
+        '
+        '
+        Me.ProgressBarX1.BackgroundStyle.Class = ""
+        Me.ProgressBarX1.Location = New System.Drawing.Point(1, 45)
+        Me.ProgressBarX1.Name = "ProgressBarX1"
+        Me.ProgressBarX1.Size = New System.Drawing.Size(427, 23)
+        Me.ProgressBarX1.TabIndex = 1
+        Me.ProgressBarX1.TextVisible = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(430, 80)
+        Me.Controls.Add(Me.ProgressBarX1)
+        Me.Controls.Add(Me.LabelX1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.Text = "PSO2 Tweaker Updater"
+        Me.TopMost = True
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents LabelX1 As DevComponents.DotNetBar.LabelX
+    Friend WithEvents ProgressBarX1 As DevComponents.DotNetBar.Controls.ProgressBarX
+
+End Class
---
+++ WindowsApplication2/Form1.vb
@@ -0,0 +1,42 @@
+﻿Imports System.Net
+
+Public Class Form1
+    Dim FreedomURL As String = "http://162.243.211.123/freedom/"
+    Public Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Visible = True
+        For Each p As Process In Process.GetProcesses
+            If p.ProcessName = "PSO2 Tweaker" Then 'If you don't know what your program's process name is, simply run your program, run windows task manager, select 'processes' tab, scroll down untill you find your programs name.
+                p.Kill()
+            End If
+        Next
+        Application.DoEvents()
+        Threading.Thread.Sleep(1000)
+        IO.File.Delete("PSO2 Tweaker.exe")
+        Dim t1 As New Threading.Thread(AddressOf UpdateTime)
+        t1.IsBackground = True
+        t1.Start()
+    End Sub
+    Private Sub UpdateTime()
+        Dim justice As New Net.WebClient
+        FreedomURL = justice.DownloadString("http://arks-layer.com/freedom.txt")
+        If FreedomURL.Contains("freedom") = False Then
+            FreedomURL = "http://162.243.211.123/freedom/"
+        End If
+        Dim client As New Net.WebClient
+        client.DownloadFileAsync(New Uri(FreedomURL & "PSO2%20Tweaker.exe"), (Application.StartupPath & "\PSO2 Tweaker.exe"))
+        AddHandler client.DownloadProgressChanged, AddressOf client_ProgressChanged
+        AddHandler client.DownloadFileCompleted, AddressOf client_DownloadCompleted
+    End Sub
+
+    Private Sub client_DownloadCompleted(sender As Object, e As System.ComponentModel.AsyncCompletedEventArgs)
+        Process.Start("PSO2 Tweaker.exe")
+        Application.Exit()
+    End Sub
+    Private Sub client_ProgressChanged(sender As Object, e As Net.DownloadProgressChangedEventArgs)
+        Dim totalSize As Long = e.TotalBytesToReceive
+        Dim downloadedBytes As Long = e.BytesReceived
+        Dim percentage As Integer = e.ProgressPercentage
+        ProgressBarX1.Value = percentage
+        ProgressBarX1.Text = "Downloaded " & downloadedBytes & "/" & totalSize & " (" & percentage & "%)"
+    End Sub
+End Class
